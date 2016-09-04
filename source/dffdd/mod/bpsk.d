fb47a1058585ed1790515d349b6c1d2ce8acd1e5
Kazuki Komatsu <komatsu@comm.ee.tut.ac.jp>
2016-09-04 10:50:59 +0900

    BPSK, QPSKの追加

---
+++ source/dffdd/mod/bpsk.d
@@ -0,0 +1,38 @@
+module dffdd.mod.bpsk;
+
+import std.complex;
+
+struct BPSK
+{
+    alias InputElementType = ubyte;
+    alias OutputElementType = Complex!float;
+
+    enum size_t symInputLength = 1;
+    enum size_t symOutputLength = 1;
+
+
+    static
+    ref OutputElementType[] modulate(in InputElementType[] inputs, return ref OutputElementType[] outputs)
+    {
+        if(outputs.length != inputs.length)
+            outputs.length = inputs.length;
+
+        foreach(i; 0 .. inputs.length)
+            outputs[i] = Complex!float(inputs[i] != 0 ? -1 : 1, 0);
+
+        return outputs;
+    }
+
+
+    static
+    ref InputElementType[] demodulate(in OutputElementType[] inputs, return ref InputElementType[] outputs)
+    {
+        if(outputs.length != inputs.length)
+            outputs.length = inputs.length;
+
+        foreach(i; 0 .. inputs.length)
+            outputs[i] = inputs[i].re > 0 ? 0 : 1;
+
+        return outputs;
+    }
+}
---
+++ source/dffdd/mod/qpsk.d
@@ -0,0 +1,44 @@
+module dffdd.mod.qpsk;
+
+import std.complex;
+
+struct QPSK
+{
+    alias InputElementType = ubyte;
+    alias OutputElementType = Complex!float;
+
+    enum size_t symInputLength = 2;
+    enum size_t symOutputLength = 1;
+
+
+    static
+    ref OutputElementType[] modulate(in InputElementType[] inputs, return ref OutputElementType[] outputs)
+    in{
+        assert(inputs.length % symInputLength == 0);
+    }
+    body{
+        if(outputs.length != inputs.length/2)
+            outputs.length = inputs.length/2;
+
+        foreach(i; 0 .. inputs.length/2)
+            outputs[i] = Complex!float(inputs[i*2+0] != 0 ? -1 : 1,
+                                       inputs[i*2+1] != 0 ? -1 : 1);
+
+        return outputs;
+    }
+
+
+    static
+    ref InputElementType[] demodulate(in OutputElementType[] inputs, return ref InputElementType[] outputs)
+    {
+        if(outputs.length != inputs.length*2)
+            outputs.length = inputs.length*2;
+
+        foreach(i; 0 .. inputs.length){
+            outputs[i*2+0] = inputs[i].re > 0 ? 0 : 1;
+            outputs[i*2+1] = inputs[i].im > 0 ? 0 : 1;
+        }
+
+        return outputs;
+    }
+}
