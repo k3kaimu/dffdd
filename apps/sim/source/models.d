00469f0d17415e0adb84770a6a32de0b75e80b45
Kazuki Komatsu <enjouzensyou.boinc@gmail.com>
2018-04-19 22:24:03 +0900

    不要なコメントを削除

--- apps/sim/source/models.d
+++ apps/sim/source/models.d
@@ -92,18 +92,6 @@ struct Model
     bool useSRXIQ = true;
     bool useSRXQZ = true;
 
-    // bool useDesiredBaseband = true;
-    // bool useTxBaseband = true;
-    // bool useTxBasebandSWP = true;
-    // bool useDesiredPADirect = true;
-    // bool usePADirect = true;
-    // bool usePADirectSWP = true;
-    // bool useReceivedDesired = true;
-    // bool useReceivedSI = true;
-    // bool useReceivedSISWP = true;
-    // bool useReceived = true;
-    // bool useNoise = true;
-
     bool doNoiseElimination = false;
 
 /*
@@ -152,23 +140,6 @@ struct Model
         }
     }
     ThermalNoise thermalNoise;
-
-
-    //struct UpDownSampler
-    //{
-    //    uint scaleOfUpSampling = 8;
-    //    immutable(real)[] decimationFIRFilterTaps = [
-    //        0.0064217,0.0030971,0.003501,0.0036559,0.0034831,0.0029178,0.0019183,0.00047411,-0.0013877,-0.003598,-0.0060443,-0.0085724,
-    //        -0.010993,-0.013088,-0.014628,-0.015381,-0.015132,-0.0137,-0.010952,-0.0068179,-0.0013004,0.0055175,0.013472,0.022324,
-    //        0.031764,0.041433,0.050934,0.059859,0.06781,0.074421,0.079383,0.082458,0.0835,0.082458,0.079383,0.074421,0.06781,
-    //        0.059859,0.050934,0.041433,0.031764,0.022324,0.013472,0.0055175,-0.0013004,-0.0068179,-0.010952,-0.0137,-0.015132,
-    //        -0.015381,-0.014628,-0.013088,-0.010993,-0.0085724,-0.0060443,-0.003598,-0.0013877,0.00047411,0.0019183,0.0029178,
-    //        0.0034831,0.0036559,0.003501,0.0030971,0.0064217];
-
-    //    size_t numOfShift = 67;
-    //    //size_t numOfShift = 0;
-    //}
-    //UpDownSampler upDownSampler;
 
 
     struct PowerSpectralDensityAnalyzer
