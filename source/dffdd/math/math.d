c7a094acd2b27500d852f3b757e6302f432dcf13
Kazuki Komatsu <komatsu@comm.ee.tut.ac.jp>
2018-05-09 16:37:54 +0900

    0次の変形ベッセル関数を実装

--- source/dffdd/math/math.d
+++ source/dffdd/math/math.d
@@ -3,6 +3,44 @@ module dffdd.math.math;
 import std.math;
 import std.traits;
 
+
+/**
+0次の変形ベッセル関数I_0(x)を計算します．
+*/
+F besselI0(F)(F x) pure nothrow @safe @nogc
+if(isFloatingPoint!F)
+{
+    if(x.isNaN) return F.nan;
+    if(x.isInfinity) return x > 0 ? F.infinity : F.nan;
+
+    x *= 0.5;
+
+    F dst = 0;
+    F r = 1;
+    size_t cnt = 0;
+    while(abs(r) > F.epsilon){
+        dst += r;
+        ++cnt;
+
+        immutable s = x / cnt;
+        r *= s * s;
+    }
+
+    return dst;
+}
+
+unittest
+{
+    // compare with numpy's result
+    assert(approxEqual(besselI0(0.0L),      1));
+    assert(approxEqual(besselI0(1.0L),      1.2660658777520082));
+    assert(approxEqual(besselI0(100.0L),    1.0737517071310738e+42));
+
+    assert(approxEqual(besselI0(0.1)-1,     0.002501562934095647));
+    assert(approxEqual(besselI0(0.01)-1,    2.5000156250509775e-05));
+}
+
+
 /**
 sin(pi * x) / (pi * x)を計算します
 */
