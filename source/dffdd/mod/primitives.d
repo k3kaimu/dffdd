3edb72f54a5a3c7a56f3aceb6e65d35d9aeb9780
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-06-25 16:11:52 +0900

    add SoftSymbolGenerator

--- source/dffdd/mod/primitives.d
+++ source/dffdd/mod/primitives.d
@@ -355,3 +355,134 @@ unittest
         assert(ser == 3.0 / 4);
     }
 }
+
+
+
+
+
+
+struct P0P1Calculator(C, F)
+{
+    this(Mod)(Mod mod)
+    in(mod.symOutputLength == 1)
+    {
+        _nbits = mod.symInputLength;
+        _p0 = new F[_nbits];
+        _p1 = new F[_nbits];
+
+        C[] sym = new C[1];
+        Bit[] bits = new Bit[_nbits];
+        foreach(n; 0 .. 1 << _nbits) {
+            foreach(k; 0 .. _nbits)
+                bits[k] = (n & (1 << k)) ? 1 : 0;
+
+            mod.modulate(bits, sym);
+            _points ~= sym[0];
+        }
+    }
+
+
+    ref F[] computeP0P1(in C[] inputs, return ref F[] p0p1, F N0)
+    {
+        p0p1.length = inputs.length * _nbits;
+
+        foreach(i, x; inputs) {
+            _p0[] = 0;
+            _p1[] = 0;
+
+            foreach(j, y; _points) {
+                auto p = exp(-(x - y).sqAbs/ N0);
+
+                foreach(k; 0 .. _nbits) {
+                    if(j & (1 << k))
+                        _p1[k] += p;
+                    else
+                        _p0[k] += p;
+                }
+            }
+
+            foreach(k; 0 .. _nbits)
+                p0p1[i * _nbits + k] = _p0[k] / _p1[k];
+        }
+
+        return p0p1;
+    }
+
+
+  private:
+    size_t _nbits;
+    C[] _points;
+    F[] _p0;
+    F[] _p1;
+}
+
+
+auto p0p1Calculator(F, Mod)(Mod mod)
+{
+    return P0P1Calculator!(Mod.OutputElementType, F)(mod);
+}
+
+
+
+struct SoftSymbolGenerator(C, R)
+{
+    this(Mod)(Mod mod)
+    in(mod.symOutputLength == 1)
+    do {
+        _nbits = mod.symInputLength;
+        _symbols = new C[2^^_nbits];
+        _tempP0 = new R[_nbits];
+        _tempP1 = new R[_nbits];
+        
+        auto bits = new Bit[2^^_nbits * _nbits];
+        foreach(n; 0 .. 2^^_nbits) {
+            foreach(j; 0 .. _nbits) {
+                bits ~= Bit(n & (1 << j));
+            }
+        }
+
+        _symbols = mod.modulate(bits, _symbols);
+    }
+
+
+    ref C[] opCall(in R[] p0p1s, return ref C[] outputs)
+    in(p0p1s.length % _nbits == 0)
+    do {
+        outputs.length = p0p1s.length / _nbits;
+
+        foreach(i; 0 .. outputs.length) {
+            outputs[i] = C(0);
+
+            foreach(j; 0 .. _nbits) {
+                _tempP1[j] = 1/(p0p1s[i * _nbits + j] + 1);
+                _tempP0[j] = 1 - _tempP1[j];
+            }
+
+            foreach(n; 0 .. 2^^_nbits) {
+                R q = 1;
+                foreach(j; 0 .. _nbits) {
+                    if(n & (1 << j))
+                        q *= _tempP1[j];
+                    else
+                        q *= _tempP0[j];
+                }
+
+                outputs[i] += q * _symbols[n];
+            }
+        }
+
+        return outputs;
+    }
+
+  private:
+    size_t _nbits;
+    C[] _symbols;
+    R[] _tempP0, _tempP1;
+}
+
+
+auto softSymbolGenerator(R, Mod)(Mod mod)
+{
+    return SoftSymbolGenerator!(Mod.OutputElementType, R)(mod);
+}
+
