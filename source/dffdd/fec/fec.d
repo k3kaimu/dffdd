8180d04d949b8ef6b2932b370397969a27cc97fc
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2021-07-27 17:40:06 +0900

    add NopBlockFec

--- source/dffdd/fec/fec.d
+++ source/dffdd/fec/fec.d
@@ -39,3 +39,42 @@ interface BlockFec(T) : Fec!(T)
         // assert(input.length / this.outputLength == r.length / this.inputLength);
     };
 }
+
+
+
+class NopBlockFec(T, alias decision) : Fec!T
+if(is(typeof(decision(T.init) ) == Bit))
+{
+    this(size_t N)
+    {
+        _size = N;
+    }
+
+
+    size_t inputLength() const { return _size; }
+    size_t outputLength() const { return _size; }
+
+
+    Bit[] encode(in Bit[] info, return ref Bit[] encoded)
+    {
+        encoded.length = info.length;
+        foreach(i; 0 .. info.length)
+            encoded[i] = info[i];
+
+        return encoded;
+    }
+
+
+    Bit[] decode(in T[] input, return ref Bit[] decoded)
+    {
+        decoded.length = input.length;
+        foreach(i; 0 .. input.length)
+            decoded[i] = decision(input[i]);
+        
+        return decoded;
+    }
+
+
+  private:
+    size_t _size;
+}
