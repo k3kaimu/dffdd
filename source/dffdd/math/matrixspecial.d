dd1601d0dcf446d1f6e2731f3c8f4420989529fb
Komatsu Kazuki <komatsu.kazuki.op@tut.jp>
2023-03-07 21:02:51 +0900

    add diagonal matrix

--- source/dffdd/math/matrixspecial.d
+++ source/dffdd/math/matrixspecial.d
@@ -363,3 +363,179 @@ Identity!(Unqual!E) identity(E)(size_t n)
 
 
 enum isIdentity(T) = is(Unqual!T == Identity!(T.ElementType));
+
+
+struct MulDiagonal(ArrayLike, MatVec)
+if(is(typeof((ArrayLike a){ auto e1 = a[0]; auto e2 = a.length; })) && (isMatrixLike!MatVec || isVectorLike!MatVec))
+{
+    alias ElementType = typeof(_diag[0] * MatVec.ElementType.init);
+    enum float exprTreeCost = 1;
+
+
+    this(ArrayLike vec, MatVec matvec)
+    {
+        _diag = vec;
+        _matvec = matvec;
+    }
+
+
+  static if(isMatrixLike!MatVec)
+  {
+    size_t length(size_t dim = 0)() const @property
+    if(dim == 0 || dim == 1)
+    {
+        return _matvec.length!dim;
+    }
+
+
+    ElementType opIndex(size_t i, size_t j) const
+    {
+        return _matvec[i, j] * _diag[i];
+    }
+  }
+  else
+  {
+    size_t length() const @property
+    {
+        return _matvec.length;
+    }
+
+
+    ElementType opIndex(size_t i) const
+    {
+        return _matvec[i] * _diag[i];
+    }
+  }
+
+
+  static if(isMatrixLike!MatVec)
+  {
+    void evalTo(T, SliceKind kindA, Alloc)(Slice!(T*, 2, kindA) dst, ref Alloc alloc) const
+    in(dst.length!0 == this.length!0 && dst.length!1 == this.length!1)
+    {
+        _matvec.evalTo(dst, alloc);
+        foreach(i; 0 .. _diag.length)
+            dst[i, 0 .. $] *= _diag[i];
+    }
+  }
+  else
+  {
+    void evalTo(T, SliceKind kindA, Alloc)(Slice!(T*, 1, kindA) dst, ref Alloc alloc) const
+    in(dst.length == this.length)
+    {
+        _matvec.evalTo(dst, alloc);
+        foreach(i; 0 .. _diag.length)
+            dst[i] *= _diag[i];
+    }
+  }
+
+
+  static if(isMatrixLike!MatVec)
+  {
+    auto _opB_Impl_(string op : "*", V)(V vec)
+    if(isVectorLike!V)
+    in(vec.length == this.length!1)
+    {
+        auto mm = _matvec * vec;
+        return MulDiagonal!(ArrayLike, typeof(mm))(_diag, mm);
+    }
+
+
+    auto _opB_Impl_(string op : "*", M)(M mat)
+    if(isMatrixLike!M)
+    in(mat.length!0 == this.length!1)
+    {
+        auto mm = _matvec * mat;
+        return MulDiagonal!(ArrayLike, typeof(mm))(_diag, mm);
+    }
+
+    mixin(definitionsOfMatrixOperators(["defaults", "M+M", "M*S"]));
+  }
+  else
+  {
+    mixin(definitionsOfVectorOperators(["defaults", "V+V", "V*S"]));
+  }
+
+
+  private:
+    ArrayLike _diag;
+    MatVec _matvec;
+}
+
+
+auto diag(Iterator, SliceKind kind)(Slice!(Iterator, 1, kind) slice)
+{
+    auto ident = identity!(typeof(slice[0]))(slice.length);
+    return MulDiagonal!(typeof(slice), typeof(ident))(slice, ident);
+}
+
+
+auto diag(E)(in E[] slice)
+{
+    auto ident = identity!(Unqual!(typeof(slice[0])))(slice.length);
+    return MulDiagonal!(const(E)[], typeof(ident))(slice, ident);
+}
+
+
+unittest
+{
+    auto ident = identity!int(3);
+    static assert(isMatrixLike!(typeof(ident)));
+
+    auto ms = slice!int(3, 3);
+
+    auto s1 = sliced([1, 2, 3]);
+    auto dmat1 = diag(s1);
+
+    assert(dmat1.length!0 == 3 && dmat1.length!1 == 3);
+    assert(dmat1[0, 0] == 1);
+    assert(dmat1[0, 1] == 0);
+    assert(dmat1[0, 2] == 0);
+    assert(dmat1[1, 0] == 0);
+    assert(dmat1[1, 1] == 2);
+    assert(dmat1[1, 2] == 0);
+    assert(dmat1[2, 0] == 0);
+    assert(dmat1[2, 1] == 0);
+    assert(dmat1[2, 2] == 3);
+
+    dmat1.evalTo(ms, theAllocator);
+    assert(ms[0, 0] == 1);
+    assert(ms[0, 1] == 0);
+    assert(ms[0, 2] == 0);
+    assert(ms[1, 0] == 0);
+    assert(ms[1, 1] == 2);
+    assert(ms[1, 2] == 0);
+    assert(ms[2, 0] == 0);
+    assert(ms[2, 1] == 0);
+    assert(ms[2, 2] == 3);
+
+    auto s2 = [1, 2, 3];
+    auto dmat2 = diag(s2);
+
+    auto mat3 = [1, 2, 3, 4, 5, 6, 7, 8, 9].sliced(3, 3).matrixed;
+    auto mul3 = dmat2 * mat3;
+    assert(mul3[0, 0] == 1);
+    assert(mul3[0, 1] == 2);
+    assert(mul3[0, 2] == 3);
+    assert(mul3[1, 0] == 8);
+    assert(mul3[1, 1] == 10);
+    assert(mul3[1, 2] == 12);
+    assert(mul3[2, 0] == 21);
+    assert(mul3[2, 1] == 24);
+    assert(mul3[2, 2] == 27);
+
+    mul3.evalTo(ms, theAllocator);
+    assert(ms[0, 0] == 1);
+    assert(ms[0, 1] == 2);
+    assert(ms[0, 2] == 3);
+    assert(ms[1, 0] == 8);
+    assert(ms[1, 1] == 10);
+    assert(ms[1, 2] == 12);
+    assert(ms[2, 0] == 21);
+    assert(ms[2, 1] == 24);
+    assert(ms[2, 2] == 27);
+
+    auto v1 = vector!int(3);
+    // auto mul4 = mul3 * v1;
+    auto mul4 = mul3._opB_Impl_!"*"(v1);
+}
