8e72b097d5ab399f8b34aba7ae4fb62d1669be65
Komatsu Kazuki <komatsu.kazuki.op@tut.jp>
2023-03-23 14:23:39 +0900

    add public imports to dffdd.math package

--- source/dffdd/math/package.d
+++ source/dffdd/math/package.d
@@ -1,4 +1,10 @@
 module dffdd.math;
 
-public import dffdd.math.math;
 public import dffdd.math.complex;
+public import dffdd.math.exprtemplate;
+public import dffdd.math.lapack;
+public import dffdd.math.linalg;
+public import dffdd.math.math;
+public import dffdd.math.matrix;
+public import dffdd.math.matrixspecial;
+public import dffdd.math.vector;
