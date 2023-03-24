93282e79be60e8ccf91aa6e2c22d7bc0b8bf132f
Komatsu Kazuki <komatsu.kazuki.op@tut.jp>
2023-03-24 14:29:49 +0900

    add SEFDM simulation

--- apps/sim_rdfts_sefdm/source/app.d
+++ apps/sim_rdfts_sefdm/source/app.d
@@ -5,6 +5,7 @@ import std.complex;
 import std.random;
 import std.range;
 import std.stdio;
+import std.traits;
 
 import dffdd.mod;
 
@@ -18,13 +19,13 @@ void main()
     // writeln("Hello");
     foreach(vEbN0dB; [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
         SimParams!(QPSK!C) params;
-        params.sefdm.nFFT = 64;
-        params.sefdm.nTone = 64;
+        params.sefdm.nFFT = 64*4*4;
+        params.sefdm.nTone = 40*4*4;
         params.sefdm.rndSeed = 0;
         params.vEbN0dB = vEbN0dB;
 
         auto simResult = runSimImpl(params);
-        writefln!"%s: %s"(vEbN0dB, simResult.ber);
+        writefln!"%s: %s, Mod: %s [Mbps], Demod: %s [Mbps]"(vEbN0dB, simResult.ber, simResult.modThroughput / 1e6, simResult.demodThroughput / 1e6);
     }
 }
 
@@ -34,6 +35,7 @@ struct SimResult
     double ber;
     long errbits;
     long totalbits;
+    double modThroughput, demodThroughput;
 }
 
 
@@ -53,12 +55,14 @@ struct SimParams(Mod)
     RDFTsSEFDMParams!Mod sefdm;
     double vEbN0dB;
     size_t maxErrbits = 100;
-    size_t minTotalbits = 10_000_000;
+    size_t minTotalbits = 100_000_000;
 }
 
 
 SimResult runSimImpl(Mod)(SimParams!Mod params)
 {
+    import std.datetime.stopwatch;
+
     immutable double SIGMA2 = 1.0 / (10^^(params.vEbN0dB/10.0) * params.sefdm.mod.symInputLength);
 
     size_t totalbits = 0;
@@ -66,19 +70,28 @@ SimResult runSimImpl(Mod)(SimParams!Mod params)
 
     size_t rndSeed = 0;
 
+    // auto sefdm = new RDFTsSEFDM!Mod(params.sefdm, SIGMA2);
+    StopWatch swMod, swDem;
+
     while(totalbits < params.minTotalbits && errbits < params.maxErrbits)
     {
         ++rndSeed;
 
         // 送信ビット系列の生成
+        swMod.start();
         Bit[] txbits;
         txbits = genBits(params.sefdm.nData * params.sefdm.mod.symInputLength, rndSeed, txbits);
+        swMod.stop();
 
         // RDFT-s-SEFDM変調
         auto txsignal = modulateSEFDM(params.sefdm, txbits);
+        // auto txsignal = sefdm.modulate(txbits);
         txsignal = txsignal.addAWGN(rndSeed, SIGMA2);
 
-        Bit[] rxbits = demodulateSEFDM(params.sefdm, txsignal);
+        swDem.start();
+        Bit[] rxbits = demodulateSEFDM(params.sefdm, txsignal, SIGMA2);
+        // auto rxbits = sefdm.demodulate(txsignal);
+        swDem.stop();
         errbits += txbits.zip(rxbits).map!"a[0] != a[1] ? 1 : 0".sum();
         totalbits += txbits.length;
     }
@@ -88,6 +101,8 @@ SimResult runSimImpl(Mod)(SimParams!Mod params)
     result.totalbits = totalbits;
     result.errbits = errbits;
     result.ber = errbits * 1.0 / totalbits;
+    result.modThroughput = totalbits * 1.0 / swMod.peek.total!"usecs" * 1e6;
+    result.demodThroughput = totalbits * 1.0 / swDem.peek.total!"usecs" * 1e6;
     return result;
 }
 
@@ -95,15 +110,83 @@ SimResult runSimImpl(Mod)(SimParams!Mod params)
 C[] modulateSEFDM(Mod)(in ref RDFTsSEFDMParams!Mod params, Bit[] txbits)
 in(txbits.length == params.mod.symInputLength * params.nData)
 {
-    C[] dst;
-    return params.mod.modulate(txbits, dst);
+    import std;
+    import dffdd.utils.fft;
+
+    C[] scmod;
+    scmod = params.mod.modulate(txbits, scmod);
+
+    // DFTスプレッド
+    auto fftw = makeFFTWObject!(TemplateOf!C)(params.nData);
+    fftw.inputs!F[] = scmod[];
+    fftw.fft!F();
+    scmod[] = fftw.outputs!F[];
+    foreach(ref e; scmod) {
+        e /= sqrt(scmod.length * 1.0);          // FFTをユニタリにする
+        e /= sqrt(params.nTone * 1.0 / params.nData);  // 1/sqrt(alpha)
+    }
+
+    // ランダムシャッフル
+    auto rnd = makeRNG(params.rndSeed, "RND_SHUF_SEFDM");
+    scmod = randomShuffle(scmod, rnd);
+
+    // 帯域圧縮
+    scmod.length = params.nFFT;
+    scmod[params.nTone .. $] = C(0);
+
+    fftw = makeFFTWObject!(TemplateOf!C)(params.nFFT);
+    fftw.inputs!F[] = scmod;
+    fftw.ifft!F();
+    scmod[] = fftw.outputs!F[];
+    foreach(ref e; scmod) {
+        e *= sqrt(scmod.length * 1.0);          // IFFTをユニタリにする
+    }
+
+    return scmod;
 }
 
 
-Bit[] demodulateSEFDM(Mod)(in ref RDFTsSEFDMParams!Mod params, C[] rxsignal)
+Bit[] demodulateSEFDM(Mod)(in ref RDFTsSEFDMParams!Mod params, C[] rxsignal, double N0)
 {
-    Bit[] dst;
-    return params.mod.demodulate(rxsignal, dst);
+    import std;
+    import mir.ndslice : sliced;
+    import dffdd.math;
+    import dffdd.detector.ep;
+    import dffdd.utils.fft;
+
+    // DFTスプレッド
+    auto W1 = dftMatrix!C(params.nData);
+
+    // ランダムシャッフル
+    auto rnd = makeRNG(params.rndSeed, "RND_SHUF_SEFDM");
+    size_t[] plist = iota(params.nData).map!"cast(size_t)a".array();
+    plist = randomShuffle(plist, rnd);
+    auto P = PermutationMatrix(plist);
+
+    // 圧縮&電力割当
+    C[] dlist = new C[params.nTone];
+    dlist[] = C(1/sqrt(params.nTone * 1.0 / params.nData));
+
+    // auto W2 = idftMatrix!C(params.nFFT);
+    // EP復調器
+    auto detector = makeSVDEPDetector!C(params.mod, identity!C(params.nTone), dlist.sliced.vectored, P * W1, N0, 20);
+
+    // 受信信号を周波数領域へ変換
+    {
+        auto fftw = makeFFTWObject!(TemplateOf!C)(params.nFFT);
+        fftw.inputs!F[] = rxsignal[];
+        fftw.fft!F();
+        rxsignal[] = fftw.outputs!F[];
+        foreach(ref e; rxsignal)
+            e /= sqrt(params.nFFT * 1.0);
+    }
+
+    C[] detected;
+    detected = detector.detect(rxsignal[0 .. params.nTone], detected);
+
+    Bit[] demodBits;
+    demodBits = params.mod.demodulate(detected, demodBits);
+    return demodBits;
 }
 
 
